8347bb184d3900178b469256a188931279b1b24f
nroehner <nicholasroehner@gmail.com>
2018-01-09 15:32:16 -0500

    Modified setup.py to depend on Git repo pySBOLx.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='xplan_to_sbol',
       version='0.1',
       description='Converter from XPLAN JSON format to SBOL.',
-      url='https://github.com/SD2E/data-representation/sbol/xplan_to_sbol_Win_64_3/xplan_to_sbol',
+      url='https://github.com/SD2E/data-representation/sbol/xplan_to_sbol',
       author='Nicholas Roehner',
       author_email='nicholasroehner@gmail.com',
       packages=['xplan_to_sbol'],
